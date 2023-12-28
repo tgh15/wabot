eefcef69587bcc5797ef800d9cf596bf4ba07bde
tgh15 <muhteguhadhiputra15@gmail.com>
2023-12-28 21:10:18 +0800

    sticker

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async(msg) => {
+    if(msg.body.startsWith('!stiker') && msg.type === "image"){
+        const media = await msg.downloadMedia()
+
+        client.sendMessage(msg.from, media, {
+            sendMediaAsSticker: true,
+            stickerAuthor: "@tgh15"
+        })
+    }
+})
+
+client.initialize();
+ 
